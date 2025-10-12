fcc4ccc9a085b8a48cb805b46663cb696c4fe77c
DavidZT2222709 <145131653+DavidZT2222709@users.noreply.github.com>
2025-10-12 11:09:17 -0500

    actulaización 3

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ import Basiceducation from './components/pages/Basiceducation.jsx'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/educational-fundation">
       <main>
         <Navbar />
 
